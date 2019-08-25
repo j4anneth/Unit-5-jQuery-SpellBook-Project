350a2d65a9fddd63d8794f3aa6e9a8514d48484b
j4anneth <42780670+j4anneth@users.noreply.github.com>
2019-08-25 13:16:56 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".zap").click(function(){
+ $(".zap").fadeOut();
+});
+
+
+
+//Spell #2
+
+$(".bird").click(function(){
+ $(".bird").slideUp();
+});
+
+
+
+//Spell #3
+
+$(".frog").click(function(){
+ $(".frog").hide();
+});
+
+
+
